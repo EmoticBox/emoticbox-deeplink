1f9b80a899ec565aec5a25983299ea685910614b
Macmini <macmini@Macminiui-Mac-mini.local>
2020-11-16 17:36:11 +0900

    deeplink test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import Deeplink from './deeplink'
+import styled from 'styled-components'
 
-const App = () => {
-  let message;
-  if (navigator.userAgent.match(/iPad/i)){
-    message = "ipad";
-  } else if (navigator.userAgent.match(/Tablet/i)){
-    message = "Tablet";
-  } else if (navigator.userAgent.match(/Android/i)){
-    message = "Android";
-  } else if (navigator.userAgent.match(/iPhone|iPod/i)){
-    message = "Iphone";
-  } else {
-    message = "PC";
+const Link = styled.a`
+  display: block;
+  margin: 1rem 0;
+  font-weight: bold;
+  font-family: consolas;
+`
+
+class deeplink extends React.Component {
+  static async getInitialProps({ store }) {
+    return {}
   }
-  return (
-    <div className="App">
-        <a href="emoticbox://">{message}</a>
-    </div>
-  );
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    // 네이버앱으로 테스트
+    this.deeplink = Deeplink({
+      iTunesLink: 'http://itunes.apple.com/kr/app/id393499958?mt=8', // ios 앱다운로드 링크
+      playStoreLink: 'http://m.androidapp.naver.com/naverapp', // android 앱다운로드 링크
+      androidPackage: 'com.nhn.android.search',
+      useFallback: true,
+      fallbackUrl: 'https://www.naver.com'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>네이버앱</h2>
+        <Link
+          onClick={() =>
+            this.deeplink.redirectToApp(`naversearchapp://default?version=1`)
+          }
+        >
+          naversearchapp://default?version=1
+        </Link>
+        <Link
+          onClick={() =>
+            this.deeplink.redirectToApp(
+              `naversearchapp://search?qmenu=voicerecg&version=1`
+            )
+          }
+        >
+          naversearchapp://search?qmenu=voicerecg&version=1
+        </Link>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default deeplink
+// import React from 'react';
+// import {Link} from 'react-router-dom';
+
+// const App = () => {
+//   let message;
+//   if (navigator.userAgent.match(/iPad/i)){
+//     message = "ipad";
+//   } else if (navigator.userAgent.match(/Tablet/i)){
+//     message = "Tablet";
+//   } else if (navigator.userAgent.match(/Android/i)){
+//     message = "Android";
+//   } else if (navigator.userAgent.match(/iPhone|iPod/i)){
+//     message = "Iphone";
+//   } else {
+//     message = "PC";
+//   }
+//   return (
+//     <div className="App">
+//         <a href="emoticbox://emoticon">{message}</a>
+//     </div>
+//   );
+// }
+
+// export default App;
